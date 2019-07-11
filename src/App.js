4c4d08458637b601c97af3c61d1aa5cca1447bee
Christopheragnus <18465803+christopheragnus@users.noreply.github.com>
2019-07-11 18:24:27 +1000

    mid update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import styled from "styled-components";
 
 const Container = styled.div`
   padding: 3rem;
+  background-color: #21d4fd;
+  background-image: linear-gradient(19deg, #21d4fd 0%, #b721ff 100%);
+  display: grid;
+  place-items: start center;
+  height: 80vh;
 `;
 
 function App() {
--- src/components/Autocomplete.js
+++ src/components/Autocomplete.js
@@ -1,76 +1,110 @@
 import React, { useState } from "react";
-import Select from "react-select";
-
+import Select, { components } from "react-select";
 import AsyncSelect from "react-select/async";
 import axios from "axios";
+import Highlighter from "react-highlight-words";
+import styled from "styled-components";
+import "./autocomplete.css";
 
 const API_URL = "http://www.omdbapi.com";
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-const searchOptions = inputValue => {
-  console.log(inputValue);
+const CustomDropdown = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
-  if (!inputValue || inputValue === " ") {
-    inputValue = "Australia";
-  }
+const Title = styled.div`
+  font-size: 1rem;
+`;
 
-  return axios
-    .get(API_URL, {
-      params: {
-        apikey: API_KEY,
-        s: inputValue
-      }
-    })
-    .then(res => {
-      console.log(res.data.Search);
-      const searchData = res.data.Search;
-
-      return searchData.map(item => {
-        let tempArr = [];
-        tempArr["value"] = item.imdbID;
-        tempArr["label"] = item.Title;
-
-        return tempArr;
-      });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-};
+const Subtitle = styled.div`
+  font-size: 0.8rem;
+  color: grey;
+`;
 
 const customStyles = {
   control: styles => ({ ...styles, borderRadius: "40px" }),
-  option: (provided, state) => ({
-    ...provided,
-    //borderBottom: "1px dotted pink"
-    display: "flex"
+  container: styles => ({ ...styles, width: "90%" }),
+  option: (styles, state) => ({
+    ...styles
+  }),
+  multiValue: (styles, { data }) => ({
+    ...styles,
+    borderRadius: "20px",
+    padding: "4px"
   })
 };
 
-
-
 function Autocomplete() {
-  const [inputValue, setInputValue] = useState({ inputValue: "" });
+  const [inputValue, setInputValue] = useState(null);
 
-  const handleInputChange = newValue => {
-    const inputValue = newValue.replace(/\W/g, "");
-    setInputValue({ inputValue });
-    return inputValue;
+  function noOptionsMessage(inputMessage) {
+    if (!inputMessage) {
+      return "No results found.";
+    }
+  }
+
+  const searchOptions = value => {
+    return axios
+      .get(API_URL, {
+        params: {
+          apikey: API_KEY,
+          s: value
+        }
+      })
+      .then(res => {
+        //console.log(res.data.Search);
+
+        if (res.data.Response === "True") {
+          const searchData = res.data.Search;
+          return searchData.map(item => {
+            let tempArr = {};
+            tempArr["value"] = item.imdbID;
+            tempArr["label"] = item.Title;
+            tempArr["year"] = item.Year;
+
+            return tempArr;
+          });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        noOptionsMessage(err);
+      });
   };
 
-  const Option = ({ label }) => <div>{`tada`}{label}</div>
+  const customOption = props => {
+    const { label, year } = props.data;
+    console.log("inside props", props);
+
+    return (
+      <CustomDropdown>
+        <components.Option {...props}>
+          <Highlighter
+            highlightClassName="highlightClass"
+            searchWords={[inputValue]}
+            textToHighlight={label}
+          >
+            <Title>{label}</Title>
+          </Highlighter>
+          <Subtitle>{year}</Subtitle>
+        </components.Option>
+      </CustomDropdown>
+    );
+  };
 
   return (
     <AsyncSelect
       cacheOptions
       defaultOptions
       isMulti
-      name="colors"
       className="basic-multi-select"
       classNamePrefix="select"
       loadOptions={searchOptions}
       styles={customStyles}
-      components={{ Option }}
+      components={{ Option: customOption }}
+      noOptionsMessage={() => noOptionsMessage()}
     />
   );
 }
